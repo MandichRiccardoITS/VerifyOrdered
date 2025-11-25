15f446026a7375f8d4f6593cf359e9afe946954e
RickyMandich <ricky.mandich@gmail.com>
2025-11-25 15:24:56 +0100

    Add project files.

---
+++ Input.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class Input
+{
+    public static string GetString(string s)
+    {
+        Console.WriteLine(s);
+        return GetString();
+    }
+
+    public static string GetString()
+    {
+        return Console.ReadLine();
+    }
+
+    public static bool GetBool(string s)
+    {
+        Console.WriteLine(s);
+        return GetBool();
+    }
+
+    public static bool GetBool()
+    {
+        try
+        {
+            return Boolean.Parse(GetString());
+        }
+        catch (FormatException)
+        {
+            Console.Error.WriteLine("devi inserire un valore booleano\t(true per sì e false per no)");
+            return GetBool();
+        }
+    }
+
+    public static int GetInt(string s)
+    {
+        Console.WriteLine(s);
+        return GetInt();
+    }
+
+    public static int GetInt()
+    {
+        try
+        {
+            return Int32.Parse(GetString());
+        }
+        catch (FormatException)
+        {
+            Console.Error.WriteLine("devi inserire un numero intero");
+            return GetInt();
+        }
+        catch (OverflowException)
+        {
+            Console.Error.WriteLine("devi inserire un numero intero di massimo 9 cifre");
+            return GetInt();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+
+namespace VerifyOrdered
+{
+    internal class Program
+    {
+        public static bool IsOrdered(int[] array, bool descending)
+        {
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                if (descending)
+                {
+                    if (array[i] < array[i + 1])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        static int[] Add(int[] oldArray, int line)
+        {
+            int[] newArray = new int[oldArray.Length + 1];
+            for (int i = 0; i < oldArray.Length; i++)
+            {
+                newArray[i] = oldArray[i];
+            }
+            newArray[newArray.Length - 1] = line;
+            return newArray;
+        }
+        static void Main(string[] args)
+        {
+            int line;
+            int[] array = new int[0];
+            bool popolating = true;
+            do
+            {
+                try
+                {
+                    line = Int32.Parse(Input.GetString($"inserisci il {array.Length + 1}° numero (se non riconosco un numero termino il programma):"));
+                    array = Add(array, line);
+                }catch(FormatException)
+                {
+                    Console.WriteLine("input non riconosciuto, smetto di aggiungere valori all'array");
+                    popolating = false;
+                }
+            } while (popolating);
+            Console.WriteLine($"l'array è ordinato in modo crescente: {IsOrdered(array, false)}");
+        }
+    }
+}
